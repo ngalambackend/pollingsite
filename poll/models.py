4480fc91dea34d3326565a80271fbb0ff80f1454
abdullahaaf <abdullahaaf1@gmail.com>
2020-01-07 17:28:39 +0700

    getting done with writing django part 2, final code on playing with API

--- poll/models.py
+++ poll/models.py
@@ -1,11 +1,27 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    publication_date = models.DateTimeField('date_published')
+    pub_date = models.DateTimeField('date_published')
+
+    # string method that used to show readable text in django admin
+    def __str__(self):
+        return self.question_text
+
+    # this method is used to check whether the question is published recently or not
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    # string method that used to show readable text in django admin
+    def __str__(self):
+        return self.choice_text
