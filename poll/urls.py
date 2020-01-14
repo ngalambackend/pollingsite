26619d800c7869bad7f2d4abfd51130d3768f00a
abdullahaaf <abdullahaaf1@gmail.com>
2020-01-14 10:32:08 +0700

    getting done with writing first django apps part 4, use generic views

--- poll/urls.py
+++ poll/urls.py
@@ -6,11 +6,17 @@ app_name = 'poll'
 
 urlpatterns = [
     # ex /poll/
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    # generic view
+    path('', views.IndexView.as_view(), name='index'),
     # ex /poll/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # generic view
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex /poll/5/results
-    path('<int:question_id>/results/', views.results, name='result'),
+    # path('<int:question_id>/result/', views.results, name='result'),
+    # generic view
+    path('<int:pk>/result/', views.ResultView.as_view(), name='result'),
     # ex /poll/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,36 +1,82 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.http import Http404
-# from django.template import loader
 
 from .models import Question
 
+# writing django apps part 4
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Choice
+# generic view
+from django.views import generic
+
+# from django.template import loader
+
 # Create your views here.
-def index(request):
-    # return HttpResponse("Welcome to my site")
+# def index(request):
+#     # return HttpResponse("Welcome to my site")
 
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('poll/index.html')
-    context = {
-        'latest_question_list' : latest_question_list
-    }
-    # return HttpResponse(template.render(context,request))
-    return render(request, 'poll/index.html', context)
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('poll/index.html')
+#     context = {
+#         'latest_question_list' : latest_question_list
+#     }
+#     # return HttpResponse(template.render(context,request))
+#     return render(request, 'poll/index.html', context)
 
-def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist!")
-    # return render(request, 'poll/detail.html', {'question': question})
+# generic view
+class IndexView(generic.ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'latest_question_list'
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'poll/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    response = "You're looking at the result of question %s."
-    return HttpResponse(response % question_id)
+# def detail(request, question_id):
+#     # return HttpResponse("You're looking at question %s." % question_id)
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist!")
+#     # return render(request, 'poll/detail.html', {'question': question})
+
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'poll/detail.html', {'question': question})
+
+# generic view
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'poll/detail.html'
+
+# def results(request, question_id):
+#     # response = "You're looking at the result of question %s."
+#     # return HttpResponse(response % question_id)
+
+#     # writing django apps part 4
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'poll/result.html', {'question': question})
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'poll/result.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # return HttpResponse("You're voting on question %s." % question_id)
+    
+    # writing django apps part 4
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'poll/detail.html', {
+            'question': question,
+            'error_message' : "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('poll:result', args=(question_id,)))
